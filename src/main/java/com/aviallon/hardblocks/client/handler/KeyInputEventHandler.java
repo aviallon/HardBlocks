de8fbe2deaa7e8efbc6f1ef19e6a33d7fd3f6265
aviallon <antoine.viallon@gmail.com>
2016-05-17 20:43:17 +0200

    added two class to handle the KeyBindings and to react to the keypresses.

---
+++ src/main/java/com/aviallon/hardblocks/client/handler/KeyInputEventHandler.java
@@ -0,0 +1,35 @@
+package com.aviallon.hardblocks.client.handler;
+
+import com.aviallon.hardblocks.client.settings.KeyBindings;
+import com.aviallon.hardblocks.reference.Key;
+import com.aviallon.hardblocks.utility.LogHelper;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.InputEvent;
+
+/**
+ * Created by antoineviallon on 17/05/2016.
+ */
+public class KeyInputEventHandler {
+    private static Key getPressedKeyBinding(){
+        if (KeyBindings.charge.isPressed()){
+            return Key.CHARGE;
+        } else if (KeyBindings.release.isPressed()){
+            return Key.RELEASE;
+        }
+        return Key.UNKNOWN;
+    }
+
+
+    @SubscribeEvent
+    public void handleKeyInputEvent(InputEvent.KeyInputEvent event){
+        Key key = getPressedKeyBinding();
+        switch(key){
+            case CHARGE:
+                LogHelper.info("Chaaarge");
+                break;
+            case RELEASE:
+                LogHelper.info("Ok, release it");
+                break;
+        }
+    }
+}
