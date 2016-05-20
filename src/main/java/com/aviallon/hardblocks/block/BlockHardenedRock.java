36599828ee431913cf30c97e615dd247d3a9baaf
aviallon <antoine.viallon@gmail.com>
2016-05-20 17:58:06 +0200

    corrected minor bug

--- src/main/java/com/aviallon/hardblocks/block/BlockHardenedRock.java
+++ src/main/java/com/aviallon/hardblocks/block/BlockHardenedRock.java
@@ -1,14 +1,14 @@
 package com.aviallon.hardblocks.block;
 
 import com.aviallon.hardblocks.creativetab.CreativeTabHB;
-import com.aviallon.hardblocks.handler.ConfigurationHandler;
+import net.minecraft.block.material.Material;
 
 /**
  * Created by antoineviallon on 15/05/2016.
  */
 public class BlockHardenedRock extends BlockHB {
     public BlockHardenedRock(){
-        super();
+        super(Material.rock);
         this.setBlockName("hardenedRock");
         this.setHardness(5);
         this.setResistance(20);
