983428bb1a97f176b98e78836ccbd20a3aee5f9f
aviallon <antoine.viallon@gmail.com>
2016-05-23 16:53:20 +0200

    Corrected main class

--- src/main/java/com/aviallon/hardblocks/HardBlocks.java
+++ src/main/java/com/aviallon/hardblocks/HardBlocks.java
@@ -8,12 +8,12 @@ import com.aviallon.hardblocks.init.Recipes;
 import com.aviallon.hardblocks.proxy.IProxy;
 import com.aviallon.hardblocks.reference.Reference;
 import com.aviallon.hardblocks.utility.LogHelper;
-import cpw.mods.fml.common.FMLCommonHandler;
-import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.SidedProxy;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.event.FMLPostInitializationEvent;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version=Reference.VERSION)
 public class HardBlocks {
